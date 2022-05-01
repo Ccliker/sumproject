a99b0cd80009b7a6ca6b3dc3a01f3863195e30e3
Ccliker <c0556781744@gmail.com>
2022-05-01 15:42:09 +0300

    tow commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,44 @@
 const exp = require('express');
+const { json } = require('express/lib/response');
 
 const app = exp();
 
-app.listen('3300',console.log('http://localhost:3300'));
+// app.listen('3300',console.log('http://localhost:3300'));
 app.use(exp.urlencoded({extended:true}))
 app.use(exp.json())
-app.set('view engine','ejs');
-app.get('/',(req,res)=>{
-    res.send(req.body);
+app.set('view engine','ejs')
+
+var net = require('net');
+var count = 0;
+
+// var server = net.createServer(function(socket) {
+const server = new net.Server();
+
+// socket.pipe(socket);  
+// hangs after 41020 writes
+server.listen(3300,function(){
+    console.log('server listen');
 })
-app.post('/setMessage',(req,res)=>{
-    const data = req.body;
-    console.log(data);
-    res.render('index',{data:data})
-})
+server.on('connection', function(socket) {
+
+    console.log('connection') ;
+    socket.write('chavy'); 
+
+
+    socket.on('data', function(chunk) {
+        console.log(chunk+"chavy!!!!!!!");
+    });
+
+    socket.on('end', function() {
+        console.info('Socket end');
+    });
+
+    socket.on('error', function(err) {
+        console.error('Socket error: ' + err);
+    });
+
+    socket.on('close', function(err) {
+        console.error('close: ');
+    });
+
+});
